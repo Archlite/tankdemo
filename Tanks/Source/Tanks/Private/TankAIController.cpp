fdfec9349d4a3175b5894d0d4501feda1442c358
Eoghan Geoghegan <e.geoghegan6@nuigalway.ie>
2018-07-15 02:15:34 +0100

    Start of controller for AI tanks

---
+++ Tanks/Source/Tanks/Private/TankAIController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+void  ATankAIController::BeginPlay() {
+	Super::BeginPlay();
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
+}
+
+
